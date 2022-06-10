ad00055df805293a9356c8b899eed824f6b778fa
jpcaulfi <jcaulfield135@gmail.com>
2022-06-09 22:53:20 -0400

    IDL

---
+++ app/src/utils/const.ts
@@ -0,0 +1,8 @@
+import { PublicKey } from '@solana/web3.js';
+import emojiExchangeProgram from './program-idl.json';
+
+const EMOJI_EXCHANGE_PROGRAM_ID = new PublicKey(
+    "9WxF3Zm9G1CeTzdYxevQvH5eBYWUVjmn9mUynju5urYK"
+)
+
+const EMOJI_EXCHANGE_PROGRAM = emojiExchangeProgram;
